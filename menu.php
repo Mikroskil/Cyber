83b6b6b09731ec50c8770aca211733b09cd4bded
admin <alfronsopurba@ymail.com>
2013-11-26 19:18:03 +0700

    Upload file menu

---
+++ menu.php
@@ -0,0 +1,6 @@
+<div class="grid_12">
+            <ul class="nav main">
+                <li class="ic-dashboard"><a href="index.php"><span>Dashboard</span></a> </li>
+               
+            </ul>
+        </div>
