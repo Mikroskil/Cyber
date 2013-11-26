36560b8aee6083006e6cb345ec4d2359bd51a962
admin <alfronsopurba@ymail.com>
2013-11-26 19:24:20 +0700

    Upload file sidebar

---
+++ sidebar.php
@@ -0,0 +1,13 @@
+<div class="box sidemenu">
+                <div class="block" id="section-menu">
+                    <ul class="section menu">
+                    
+                    	<li><a href=".">Dashboard</a></li>
+                        <li><a href="makanan.php">Menu Makanan</a></li>
+                        <li><a href="minuman.php">Menu Minuman</a></li>
+                        <li><a href="meja.php">Meja</a></li>
+                        <li><a href="pesanan.php">Menu Pesanan</a></li>
+                        <li><a href="kasir.php">Kasir</a></li>
+                    </ul>
+                </div>
+            </div>
